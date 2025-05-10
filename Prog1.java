577717027a37bcb2b2206fa3dcbe4902c38d5a56
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-10 19:42:51 +0530

    Add files via upload

---
+++ Prog1.java
@@ -0,0 +1,10 @@
+//Signing in
+
+public class Prog1{
+        public static void main(String[] args) {
+        //Write your code here 
+        System.out.println("Hello Spy");
+    }
+}
+
+//Signing off  
