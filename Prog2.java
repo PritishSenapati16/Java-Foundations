cb232bdddfd09a93125ada232008202164fb1b5d
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-11 23:47:37 +0530

    Add files via upload

---
+++ Prog2.java
@@ -0,0 +1,15 @@
+// Q -- Write a java program to add 3 numbers.
+
+// ANS ---
+
+//Signing in
+public class Prog2 {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 7;
+        int num3 = 2;
+        int sum = num1 + num2 + num3;
+        // Adding 3 numbers
+        System.out.println(sum);
+    }
+}
