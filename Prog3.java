58e40c96fe365f720f367693001c8ff7b1f28b6c
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-11 23:47:50 +0530

    Add files via upload

---
+++ Prog3.java
@@ -0,0 +1,18 @@
+// Signing in
+public class Prog3 {
+    public static void main(String[] args) {
+        byte age = 52;
+        int age1 = 36;
+        long age2 = 5676547L;
+        char ch = 'P';
+        float f1 = 3.6f;
+        double d1 = 3.66d;
+        System.out.println(age);
+        System.out.println(age1);
+        System.out.println(age2);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+    }
+}
+// Signing off
