59ae68f9b64575faee7cc1c381fac908c2855244
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-12 21:33:22 +0530

    Add files via upload

---
+++ Prog9.java
@@ -0,0 +1,22 @@
+// Signing in 
+public class Prog9 {
+    public static void main(String[] args) {
+         byte x = 5;
+         int y = 6;
+         short z = 8;
+         int a = y+z;
+         float b = 6.54f+x;
+         System.out.println(a);
+         System.out.println(b);
+        // Increment and Decrement Operators
+        int i = 76;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        char ch = 's';
+        System.out.println(++ch);
+        System.out.println("END OF THE PROGRAM");
+    }
+}
+// Signing off  
