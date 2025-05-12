b6eb8da9a0fe6e70b0e67a243524e868077dd507
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-12 21:32:38 +0530

    Add files via upload

---
+++ Prog7.java
@@ -0,0 +1,13 @@
+// Signing in
+public class Prog7 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 6+a;
+        System.out.println(b);
+        System.out.println(77>9 & 77<8);
+        System.out.println(64>5 || 76<7);
+        System.out.println(7&8);
+        System.out.println("End of the program");
+    }
+}
+// Signing off
