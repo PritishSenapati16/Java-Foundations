50bf65f333b739d3e07ed79f56a7b70dc0ea95a7
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-12 21:33:05 +0530

    Add files via upload

---
+++ Prog8.java
@@ -0,0 +1,22 @@
+// Signing in
+public class Prog8 {
+    public static void main(String[] args) {
+    // Precedence and Associativity
+        int a = 6*5-34/2;
+        /*
+           =30-34/2
+           =30-17
+           =13
+        */
+        int b = 60/5-34*2;
+        /*
+           =12-34*2
+           =12-68
+           =-56
+         */
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("END OF THE PROGRAM");
+    }
+}
+// Signing off  
