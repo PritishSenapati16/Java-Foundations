d6d9111fe89608efc4faee5f75dab20dc87bffce
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-14 16:06:30 +0530

    Add files via upload

---
+++ Prog10.java
@@ -0,0 +1,12 @@
+// Signing in
+public class Prog10 {
+    public static void main(String[] args) {
+        String name = "Crusader";
+        System.out.println(name);
+        int a = 7;
+        float b = 6.78f;
+        System.out.printf("The value of a is %d and value of b is %f",a,b);   
+        System.out.format("The value of a is %d and value of b is %f",a,b); 
+     }
+}
+// Signing off 
