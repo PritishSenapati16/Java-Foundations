f1afb13691d3f5b49c35e6fc71e7c9f8b1ba22a9
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-15 05:27:52 +0530

    Add files via upload

---
+++ Prog16.java
@@ -0,0 +1,18 @@
+// Signing in 
+public class Prog16 {
+    public static void main(String[] args) {
+        int age = 19;
+
+        switch(age){
+            case 18:
+               System.out.println("You Are An Adult");
+               break;
+            case 25:
+               System.out.println("You Are Going To Join Your Job");
+            break;
+            default:
+               System.out.println("Enjoy");
+        }
+    }
+}
+// Signing off
