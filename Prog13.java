a75e4f9eab1aed17ebb8485b9918c4c1d9d008cc
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-14 16:07:58 +0530

    Add files via upload

---
+++ Prog13.java
@@ -0,0 +1,14 @@
+// Signing in
+public class Prog13 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        if (a&&b){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
+// Signing off 
