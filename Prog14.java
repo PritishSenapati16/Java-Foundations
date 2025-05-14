d1348a48bd92b1a8781ae96efeec5336fb45efd0
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-15 05:25:31 +0530

    Add files via upload

---
+++ Prog14.java
@@ -0,0 +1,19 @@
+// Signing in
+public class Prog14 {
+    public static void main(String[] args) {
+        int age = 56;
+        if (age>36){
+            System.out.println("You are noob");
+        }
+        else if(age>46){
+            System.out.println("You are pro");
+        }
+        else if(age>66){
+            System.out.println("You are hacker");
+        }
+        else{
+            System.out.println("Get out");
+        }
+    }
+}
+// Signing off
