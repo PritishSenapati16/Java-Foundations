993a74ce7ef379fe7e1b562febb2173dc1c72674
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-15 05:28:11 +0530

    Add files via upload

---
+++ Prog17.java
@@ -0,0 +1,21 @@
+// Signing in 
+public class Prog17 {
+    public static void main(String[] args) {
+        String name = "Crusader";
+
+        switch(name){
+            case "Crusader" ->{
+                System.out.println("A Great Person");
+                System.out.println("The Greenest Flag");
+                System.out.println("Love You");
+            }
+            case "TPP" ->{
+                System.out.println("Good Person");
+            }
+            default ->{
+                System.out.println("I am Kriss");
+            }
+        }
+    }
+}
+// Signing off
