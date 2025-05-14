b2d501c5463c09ddda22d2dd9a509fcb89dc5d53
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-14 16:07:03 +0530

    Add files via upload

---
+++ Prog12.java
@@ -0,0 +1,13 @@
+// Signing in
+public class Prog12 {
+    public static void main(String[] args) {
+        int age = 9;
+        if(age>18){
+            System.out.println("Yes boy u can drive!");
+        }
+        else{
+            System.out.println("No boy u cannot drive yet");
+        }
+    }
+}
+// Signing off
