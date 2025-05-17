49b4ca545466f4671aa87caa33ad20b0591339bd
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-17 20:44:45 +0530

    Add files via upload

---
+++ Prog18.java
@@ -0,0 +1,12 @@
+// Signing in 
+public class Prog18 {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=6){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finish Running While Loop!");
+    }
+}
+// Signing off 
