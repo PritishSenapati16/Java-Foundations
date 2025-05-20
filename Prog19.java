9725fe55620b4b218df493602906f8e58cb40671
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-20 14:07:48 +0530

    Add files via upload

---
+++ Prog19.java
@@ -0,0 +1,11 @@
+// Signing in
+public class Prog19 {
+    public static void main(String[] args) {
+        int a = 10;
+        do { 
+            System.out.println(a);
+            a++;
+        } while (a<6);
+    }
+}
+// Signing off 
