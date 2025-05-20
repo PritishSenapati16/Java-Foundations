259b21afc5b2839ec9f749beaa55ae764cedbf65
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-20 14:08:13 +0530

    Add files via upload

---
+++ Ps1Q1.java
@@ -0,0 +1,11 @@
+// Signing in
+public class Ps1Q1{
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+        int c = 9;
+        int sum = a+b+c;
+        System.out.println(sum);
+    }
+}
+// Signing off
