93a6e98ef8a585da88af44805121a05d2c665ad2
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-20 14:08:32 +0530

    Add files via upload

---
+++ Ps1Q2.java
@@ -0,0 +1,11 @@
+// Signing in 
+public class Ps1Q2 {
+    public static void main(String[] args) {
+    float subject1 = 96;
+    float subject2 = 99;
+    float subject3 = 94;
+    float cgpa = (subject1 + subject2 + subject3)/30;
+    System.out.println(cgpa);
+    }
+}
+// Signing off
