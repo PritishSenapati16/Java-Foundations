418eb1076a8060b2222e8f74bbb5b17e43d36d91
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-21 23:08:58 +0530

    Add files via upload

---
+++ Ps1Q5.java
@@ -0,0 +1,11 @@
+// Signing in
+import java.util.Scanner;
+public class Ps1Q5 {
+   public static void main(String[] args) {
+    System.out.println("Enter Your Number");
+       Scanner sc = new Scanner(System.in);
+       System.out.println(sc.hasNextInt());
+       sc.close();
+   }
+}
+// Signing off
