c4d6132063aab56494583057a1cc4e5fed046bea
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-21 23:09:40 +0530

    Add files via upload

---
+++ Ps2Q1.java
@@ -0,0 +1,9 @@
+// Signing in
+public class Ps2Q1 {
+    public static void main(String[] args) {
+        float a = 7/4*9/2;
+        System.out.println(a);
+        System.out.println("END OF THE PROGRAM");
+    }
+}
+// Signing off
