14f0898d66acaa417a9030b3a784fbc99059a29b
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-25 01:54:06 +0530

    Add files via upload

---
+++ Ps2Q3.java
@@ -0,0 +1,11 @@
+// Signing in
+import java.util.Scanner;
+public class Ps2Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a>19);
+        System.out.println("END OF THE PROGRAM");
+    }
+}
+// Signing off
