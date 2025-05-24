7d3e4f3a6a47700d52fde751b1907efcb9223ed4
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-25 02:02:02 +0530

    Add files via upload

---
+++ Ps2Q5.java
@@ -0,0 +1,9 @@
+// Signing in
+public class Ps2Q5 {
+    public static void main(String[] args) {
+        int x = 7;
+    int a = 7 * 49 / 7 + 35 / 7;
+    System.out.println(a);
+    }   
+}
+// Signing off
