25f46c0a0e4f60be97430a728ff81071ddf61c35
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-25 01:53:47 +0530

    Add files via upload

---
+++ Ps2Q2.java
@@ -0,0 +1,14 @@
+// Signing in
+public class Ps2Q2 {
+    public static void main(String[] args) {
+        char grade = 'B';
+        grade = (char)(grade + 8);
+        System.out.println(grade);
+
+        // Decrypting the grade
+        grade = (char)(grade - 8);
+        System.out.println(grade);
+        System.out.println("END OF THE PROGRAM");
+    }
+}
+// Signing off
