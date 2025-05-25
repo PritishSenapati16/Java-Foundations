24d9169543696096f405f8a79979e7026e8e9382
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-25 21:31:41 +0530

    Add files via upload

---
+++ Ps3Q4.java
@@ -0,0 +1,9 @@
+// Signing in
+public class Ps3Q4 {
+    public static void main(String[] args) {
+        String name = "I Want  Peace   in my life gpd";
+        System.out.println(name.indexOf("  "));
+        System.out.println(name.indexOf("   "));
+    }
+}
+// Signing off
