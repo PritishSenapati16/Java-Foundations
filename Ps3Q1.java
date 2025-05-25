98550a5cc04441e651d1e018b3d322496995dcc9
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-25 21:30:45 +0530

    Add files via upload

---
+++ Ps3Q1.java
@@ -0,0 +1,8 @@
+// Signing in
+public class Ps3Q1 {
+    public static void main(String[] args) {
+        String name = "PrItIsH";
+        System.out.println(name.toLowerCase());
+    }
+}
+// Signing off
