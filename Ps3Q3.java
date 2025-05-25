3354420d0d61c5efff172b469f954ec269cd5664
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-25 21:31:21 +0530

    Add files via upload

---
+++ Ps3Q3.java
@@ -0,0 +1,8 @@
+// Signing in 
+public class Ps3Q3 {
+    public static void main(String[] args) {
+        String letter = "Dear <|name|>, Thanks a lot!";
+        System.out.println(letter.replace("name","Crusader"));
+  }
+}
+// Signing off
