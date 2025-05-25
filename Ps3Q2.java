1745374463335bcb1b4eec9d0888ff4220e847ef
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-25 21:31:06 +0530

    Add files via upload

---
+++ Ps3Q2.java
@@ -0,0 +1,8 @@
+// Signing in 
+public class Ps3Q2 {
+    public static void main(String[] args) {
+        String name = "Rengoku Kyojuro";
+        System.out.println(name.replace(" ", "_"));
+  }
+}
+// Signing off
