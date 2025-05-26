b9e006e3dc3d86069b1c1ce855207a20bd6186a7
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-26 21:23:38 +0530

    Add files via upload

---
+++ Ps3Q5.java
@@ -0,0 +1,6 @@
+public class Ps3Q5 {
+    public static void main(String[] args) {
+        String letter = "Dear Harry ,\n    This Java course is Nice.\n Thanks!";
+        System.out.println(letter);
+    }
+}
