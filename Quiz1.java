44eace1ad38516e1a9a7999dbabe72c773b393cd
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-05-28 03:10:10 +0530

    Add files via upload

---
+++ Quiz1.java
@@ -0,0 +1,9 @@
+// Signing in 
+public class Quiz1 {
+    public static void main(String[] args) {
+        int y = 7;
+        int x = ++y*8;
+        System.out.println(x);
+    }
+}
+// Signing off
