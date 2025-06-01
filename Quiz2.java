d0eb61055dcb2a2ae9ab09b761d3e93cb92b1b9a
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-06-01 10:57:55 +0530

    Add files via upload

---
+++ Quiz2.java
@@ -0,0 +1,12 @@
+// Signing in 
+public class Quiz2 {
+    public static void main(String[] args) {
+        int i = 100;
+        while(i<=200){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finishing my while loop!");
+    }
+}
+// Signing off
